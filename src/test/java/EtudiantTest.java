dcd592eaaf5c9d7b5f58e40137dd0f77f2ab4097
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-12-16 14:11:12 +0100

    creat test

---
+++ src/test/java/EtudiantTest.java
@@ -0,0 +1,2 @@
+public class EtudiantTest {
+}
--- src/test/java/PointageTest.java
+++ src/test/java/PointageTest.java
@@ -1,13 +1,2 @@
-import com.youcode.systemepointage.model.Utilisateur;
-import com.youcode.systemepointage.service.PointageService;
-import com.youcode.systemepointage.service.UtilisateurService;
-
 public class PointageTest {
-    public void testpointer(){
-        UtilisateurService utilisateurService = new UtilisateurService();
-        Utilisateur hassan = utilisateurService.trouverUtilisateur("cbenhzaine@gmail.com");
-
-        PointageService pointer = new PointageService();
-        pointer.pointer(hassan);
-    }
 }
