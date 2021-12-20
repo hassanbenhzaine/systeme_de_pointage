949e0d08a971d9ea3b81c8b229eb27138955b6ee
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-12-20 09:22:20 +0100

    test

--- src/main/java/com/youcode/systemepointage/Main.java
+++ src/main/java/com/youcode/systemepointage/Main.java
@@ -9,29 +9,7 @@ import com.youcode.systemepointage.service.UtilisateurService;
 public class Main {
 
     public static void main(String[] args){
-        RoleService role = new RoleService();
-        Role adminRole = role.trouverRole("ADMIN");
 
-        Adresse hassanaddress = Adresse.builder()
-                .adresse1("avenue de la liberté")
-                .adresse2("20").codePostal(12030)
-                .ville("Maroc")
-                .pays("Rabat-sale-kenitra")
-                .build();
-
-        UtilisateurService utilisateurService = new UtilisateurService();
-        utilisateurService.seEnregistrer(
-                Utilisateur.builder()
-                        .nom("hassan")
-                        .prenom("benhzaine")
-                        .email("cbenhzaine@gmail.com")
-                        .motDePasse("123456")
-                        .telephone("0612345678")
-                        .statut(true)
-                        .build(),
-                hassanaddress,
-                adminRole
-        );
 
     }
 }
--- src/main/java/com/youcode/systemepointage/service/EvenementService.java
+++ src/main/java/com/youcode/systemepointage/service/EvenementService.java
@@ -1,4 +1,25 @@
 package com.youcode.systemepointage.service;
 
+import com.youcode.systemepointage.dao.EvenementDAO;
+import com.youcode.systemepointage.dao.EvenementDAOImp;
+import com.youcode.systemepointage.dao.PointageDAOImp;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class EvenementService {
+    private final EvenementDAO evenementDAO;
+    private static PointageService instance;
+/*
+    public static EvenementService getInstance(){
+        if(instance == null){
+            instance = new EvenementService();
+        }
+        return instance;
+    }
+    private EvenementService() {
+        evenementDAO = new EvenementDAOImp();
+    }
+
+ */
+
 }
