3314b44f95e87d4f8c64cd5b5bda25d25aa85c4a
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-12-24 09:50:52 +0100

    crud in specialite

--- src/main/java/com/youcode/systemepointage/dao/PointageDAOImp.java
+++ src/main/java/com/youcode/systemepointage/dao/PointageDAOImp.java
@@ -1,27 +1,32 @@
 package com.youcode.systemepointage.dao;
 
 import com.youcode.systemepointage.model.Pointage;
+import com.youcode.systemepointage.model.Pointage;
+import com.youcode.systemepointage.model.Utilisateur;
 import com.youcode.systemepointage.shared.ConnectionFactory;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
-    private final String TABLE_NAME = "Pointage";
+    private final String tableName = "Pointage";
+    private final GenericDAO<Utilisateur, Integer> utilisateurDAO = new UtilisateurDAOImp();
 
     @Override
     public Pointage create(Pointage pointage) {
-        String sql = "INSERT INTO \"" + TABLE_NAME + "\" (\"dateEtHeure\", \"utilisateurId\")" +
+        String sql = "INSERT INTO \"" + tableName + "\" (\"dateEtHeure\", \"utilisateurId\")" +
                 " VALUES (?, ?)";
 
-        try (Connection connection = ConnectionFactory.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
-
-            preparedStatement.setTimestamp(1, Timestamp.valueOf(pointage.getDateEtHeure()));
-            preparedStatement.setInt(2, pointage.getUtilisateurId());
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+             preparedStatement.setDate(1, Date.valueOf(pointage.getDateEtHeure().toLocalDate()));
+             preparedStatement.setInt(2, pointage.getUtilisateur().getId());
 
             preparedStatement.executeUpdate();
 
@@ -38,9 +43,9 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
 
     @Override
     public Optional<Pointage> find(Integer id) {
-        String sql = "SELECT * FROM \"" + TABLE_NAME + "\" WHERE \"id\" = ?";
+        String sql = "SELECT * FROM \"" + tableName + "\" WHERE \"id\" = ?";
 
-        try (Connection connection = ConnectionFactory.getConnection();
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setInt(1, id);
@@ -53,7 +58,7 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
                             resultSet.getDate("dateEtHeure").toLocalDate(),
                             resultSet.getTime("DateEtHeure").toLocalTime())
                     );
-                    pointage.setUtilisateurId(resultSet.getInt("utilisateurId"));
+                    pointage.setUtilisateur(utilisateurDAO.find(resultSet.getInt("utilisateurId")).get());
                     return Optional.of(pointage);
                 }
             }
@@ -65,10 +70,10 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
 
     @Override
     public List<Pointage> findAll() {
-        String sql = "SELECT * FROM \"" + TABLE_NAME + "\"";
+        String sql = "SELECT * FROM \"" + tableName + "\"";
         List<Pointage> pointages = new ArrayList<>();
 
-        try (Connection connection = ConnectionFactory.getConnection();
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -76,9 +81,11 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
             while (resultSet.next()) {
                 Pointage pointage = new Pointage();
                 pointage.setId(resultSet.getInt("id"));
-                pointage.setDateEtHeure(resultSet.getTimestamp("dateEtHeure").toLocalDateTime());
-
-                pointage.setUtilisateurId(resultSet.getInt("utilisateurId"));
+                pointage.setDateEtHeure(LocalDateTime.of(
+                        resultSet.getDate("dateEtHeure").toLocalDate(),
+                        resultSet.getTime("DateEtHeure").toLocalTime())
+                );
+                pointage.setUtilisateur(utilisateurDAO.find(resultSet.getInt("utilisateurId")).get());
 
                 pointages.add(pointage);
             }
@@ -91,13 +98,13 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
 
     @Override
     public Pointage update(Pointage pointage) {
-        String sql = "UPDATE \"" + TABLE_NAME + "\" SET \"dateEtHeure\" = ?, \"utilisateurId\" = ? WHERE id = ?";
+        String sql = "UPDATE \"" + tableName + "\" SET dateEtHeure = ?, utilisateurId = ? WHERE id = ?";
 
-        try (Connection connection = ConnectionFactory.getConnection();
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setDate(1, Date.valueOf(pointage.getDateEtHeure().toLocalDate()));
-            preparedStatement.setInt(2, pointage.getUtilisateurId());
+            preparedStatement.setInt(2, pointage.getUtilisateur().getId());
             preparedStatement.setInt(3, pointage.getId());
 
             preparedStatement.executeUpdate();
@@ -109,9 +116,9 @@ public class PointageDAOImp implements GenericDAO<Pointage, Integer> {
 
     @Override
     public boolean delete(Integer id) {
-        String sql = "DELETE FROM \"" + TABLE_NAME + "\" WHERE id = ?";
+        String sql = "DELETE FROM \"" + tableName + "\" WHERE id = ?";
 
-        try (Connection connection = ConnectionFactory.getConnection();
+        try (Connection connection = ConnectionFactory.getInstance().getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setInt(1, id);
