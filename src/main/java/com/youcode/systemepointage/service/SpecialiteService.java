134e37c1e746c5021e1a15b8a82794cba313efc4
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-12-24 09:50:52 +0100

    crud in specialite

--- src/main/java/com/youcode/systemepointage/service/SpecialiteService.java
+++ src/main/java/com/youcode/systemepointage/service/SpecialiteService.java
@@ -4,11 +4,29 @@ import com.youcode.systemepointage.dao.GenericDAO;
 import com.youcode.systemepointage.dao.SpecialiteDAOImp;
 import com.youcode.systemepointage.model.Specialite;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 
+import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 public class SpecialiteService {
     private final GenericDAO<Specialite, Integer> specialiteDAO = new SpecialiteDAOImp();
+
+    public Specialite create(Specialite specialite){
+        return specialiteDAO.create(specialite);
+    }
+
+    public Specialite update(Specialite specialite){
+        return specialiteDAO.update(specialite);
+    }
+
+    public Optional<Specialite> find(int id){
+        return specialiteDAO.find(id);
+    }
+
+    public List<Specialite> findAll(){
+        return (List<Specialite>) specialiteDAO.findAll();
+    }
 
 //    public Specialite trouver(Specialite specialite) {
 //        return specialiteDAO.find(specialite.getId())
