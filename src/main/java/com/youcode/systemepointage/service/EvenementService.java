bcab0adef9f2220d44bf16d88dea11dbb8489e5b
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-12-24 09:50:52 +0100

    crud in specialite

--- src/main/java/com/youcode/systemepointage/service/EvenementService.java
+++ src/main/java/com/youcode/systemepointage/service/EvenementService.java
@@ -1,51 +1,34 @@
 package com.youcode.systemepointage.service;
 
-import com.youcode.systemepointage.dao.EvenementDAOImp;
 import com.youcode.systemepointage.dao.GenericDAO;
 import com.youcode.systemepointage.model.Evenement;
-import com.youcode.systemepointage.model.JournalEvenement;
-import com.youcode.systemepointage.model.Utilisateur;
+import lombok.AllArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
-
+@AllArgsConstructor
 public class EvenementService {
-    private final GenericDAO<Evenement, Integer> evenementDAO = new EvenementDAOImp();
-    private final JournalEvenementService journalEvenementService = new JournalEvenementService();
+    private final GenericDAO<Evenement, Integer> evenementDAO;
     // private static PointageService instance;
 
-    public Evenement create(Evenement evenement, Utilisateur utilisateur){
-        Evenement createdEvenement = evenementDAO.create(evenement);
-        JournalEvenement journalEvenement = JournalEvenement.builder()
-                .evenement(createdEvenement)
-                .utilisateur(utilisateur)
-                .ajoute(LocalDateTime.now())
-                .build();
-        journalEvenementService.ajouter(journalEvenement);
-        return createdEvenement;
+    public Evenement create(Evenement evenement){
+        return evenementDAO.create(evenement);
     }
 
-    public Evenement update(Evenement evenement, Utilisateur utilisateur){
-        Evenement updateEvenement = evenementDAO.update(evenement);
-        JournalEvenement journalEvenement = JournalEvenement.builder()
-                .evenement(updateEvenement)
-                .utilisateur(utilisateur)
-                .modification(LocalDateTime.now())
-                .build();
-        journalEvenementService.modifier(journalEvenement);
-        return updateEvenement;
+    public Evenement update(Evenement evenement){
+        return evenementDAO.update(evenement);
     }
 
-    public Evenement find(Integer id){
-        return evenementDAO.find(id).orElse(null);
+    public Optional<Evenement> find(int id){
+        return evenementDAO.find(id);
     }
 
-    public Collection<Evenement> findAll(){
-        return evenementDAO.findAll();
+    public List<Evenement> findAll(){
+        return (List<Evenement>) evenementDAO.findAll();
      }
 
-    public boolean delete(Integer id) {
+    public boolean delete(int id) {
         return evenementDAO.delete(id);
      }
 
