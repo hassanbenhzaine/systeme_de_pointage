85a191b107d8e6d29d849bab9bead65ea5688cd2
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-01-01 20:42:22 +0100

    JournalEvenementDAOImp

--- src/main/java/com/youcode/systemepointage/service/ChefFabriqueService.java
+++ src/main/java/com/youcode/systemepointage/service/ChefFabriqueService.java
@@ -1,8 +1,10 @@
 package com.youcode.systemepointage.service;
 
 import com.youcode.systemepointage.dao.ChefFabriqueDAOImpl;
+import com.youcode.systemepointage.dao.EvenementDAOImp;
 import com.youcode.systemepointage.dao.GenericDAO;
 import com.youcode.systemepointage.model.ChefFabrique;
+import com.youcode.systemepointage.model.Evenement;
 import com.youcode.systemepointage.model.Utilisateur;
 
 import java.util.Collection;
@@ -10,10 +12,15 @@ import java.util.Collection;
 
 public class ChefFabriqueService extends UtilisateurService{
     private final GenericDAO<ChefFabrique, Integer> chefFabriqueDAO = new ChefFabriqueDAOImpl();
+    private final GenericDAO<Evenement, Integer> evenementDAO = new EvenementDAOImp();
 
     @Override
     public ChefFabrique ajouter(Utilisateur chefFabrique) {
         return chefFabriqueDAO.create((ChefFabrique) chefFabrique);
+    }
+
+    public Evenement ajouter(Evenement evenement){
+        return evenementDAO.create(evenement);
     }
 
     @Override
