b51ee0ec761e140bf33c1351a16e0c6a45062285
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-01-01 20:42:22 +0100

    JournalEvenementDAOImp

--- src/main/java/com/youcode/systemepointage/service/JournalEvenementService.java
+++ src/main/java/com/youcode/systemepointage/service/JournalEvenementService.java
@@ -1,4 +1,24 @@
 package com.youcode.systemepointage.service;
 
+import com.youcode.systemepointage.dao.GenericDAO;
+import com.youcode.systemepointage.dao.JournalEvenementDAOImp;
+import com.youcode.systemepointage.model.JournalEvenement;
+
+import java.util.Collection;
+
 public class JournalEvenementService {
+    private final GenericDAO<JournalEvenement, Integer> journalEvenementDAO = new JournalEvenementDAOImp();
+
+    public boolean supprimer(JournalEvenement journalEvenement){
+        return journalEvenementDAO.delete(journalEvenement.getId());
+    }
+    public JournalEvenement modifier(JournalEvenement journalEvenement){
+        return journalEvenementDAO.update(journalEvenement);
+    }
+    public JournalEvenement trouverParId(int id){
+        return journalEvenementDAO.find(id).orElse(null);
+    }
+    public Collection<JournalEvenement> trouverTout(){
+        return journalEvenementDAO.findAll();
+    }
 }
